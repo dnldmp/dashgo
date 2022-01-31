d67735b1bec6e86b453451d9cd6519ba6c610b3c
Danilo Dominoni <dominoni@Danilos-MacBook-Air.local>
2022-01-31 18:42:42 -0300

    utilizando mutation do react query

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -6,6 +6,10 @@ import { SubmitHandler, useForm } from 'react-hook-form'
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 import Link from 'next/link'
+import { useMutation } from 'react-query'
+import { api } from "../../services/api";
+import { queryClient } from '../../services/queryClient'
+import { useRouter } from 'next/router'
 
 type CreateUserFormData = {
   name: string;
@@ -25,6 +29,24 @@ const createUserFormSchema = yup.object().shape({
 })
 
 export default function CreateUser(){
+  const router = useRouter()
+
+  const createUser = useMutation(async (user: CreateUserFormData) => {
+    const response = await api.post('users', {
+      user: {
+        ...user,
+        created_at: new Date()
+      }
+    })
+
+    return response.data.user
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('users')
+      router.push('/users')
+    }
+  })
+
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(createUserFormSchema)
   })
@@ -32,8 +54,7 @@ export default function CreateUser(){
   const { errors } = formState
 
   const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
-    await new Promise(resolve => setTimeout(resolve, 2000))
-    console.log(values);
+    await createUser.mutateAsync(values)
   }
 
   return(
