6e8e456f728c3404bc11ab1b9c710b37d3bf650e
Danilo Dominoni <dominoni@Danilos-MacBook-Air.local>
2022-01-31 18:14:30 -0300

    prefetch de dados quando passa o mouse em cima do usuário

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,6 +5,7 @@ import {
   Flex,
   Heading,
   Icon,
+  Link,
   Spinner,
   Table,
   Tbody,
@@ -19,9 +20,11 @@ import { RiAddLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
-import Link from 'next/link'
+import NextLink from 'next/link'
 import { useUsers } from "../../services/hooks/useUsers";
 import { useState } from "react";
+import { queryClient } from "../../services/queryClient";
+import { api } from "../../services/api";
 
 export default function UserList(){
   const [ page, setPage ] = useState(1)
@@ -31,6 +34,15 @@ export default function UserList(){
     base: false,
     lg: true
   })
+
+  async function handlePrefetchUser(userId: string) {
+    await queryClient.prefetchQuery(['user', userId], async () => {
+      const response = await api.get(`/users/${userId}`)
+      return response.data
+    }, {
+      staleTime: 1000 * 60 * 10 // 10 minutos
+    })
+  }
 
   return(
     <Box>
@@ -50,7 +62,7 @@ export default function UserList(){
                 />
               }
             </Heading>
-            <Link href="/users/create" passHref>
+            <NextLink href="/users/create" passHref>
               <Button 
                 as="a"
                 size="sm"
@@ -60,7 +72,7 @@ export default function UserList(){
               >
                 Criar novo
               </Button>
-            </Link>
+            </NextLink>
           </Flex>
         
           { isLoading ? (
@@ -92,7 +104,12 @@ export default function UserList(){
                         </Td>
                         <Td>
                           <Box>
-                            <Text fontWeight="bold">{user.name}</Text>
+                            <Link 
+                              color='purple.400' 
+                              onMouseEnter={() => handlePrefetchUser(user.id)}
+                            >
+                              <Text fontWeight="bold">{user.name}</Text>
+                            </Link>
                             <Text fontSize="sm" color="gray.300">{user.email}</Text>
                           </Box>
                         </Td>
